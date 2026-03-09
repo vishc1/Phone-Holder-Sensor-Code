ee10be7529cd3148423f12a79928ace9eb5bc159
Vishwesh Chinthukumar <vishweshck3@gmail.com>
2026-03-08 21:21:41 -0700

    Update MinCalc17R4(DEMO).ino

--- MinCalc17R4(DEMO).ino
+++ MinCalc17R4(DEMO).ino
@@ -1,8 +1,7 @@
 void setup() {
-  // 1. TELL ARDUINO TO USE THE PIN, NOT THE INTERNAL 5V
+  
   analogReference(AR_EXTERNAL); 
-  //analogReference(EXTERNAL); 
-  // 2. WAIT A MOMENT FOR VOLTAGE TO SETTLE
+ 
   delay(1000); 
   
   Serial.begin(9600);
@@ -18,11 +17,11 @@ void loop() {
   float VoutMin = 4;
 
   for (i = 0; i < rangeCount;i++) {
-  // Now, 1023 represents 3.3V instead of 5V
+ 
     rawValue = analogRead(A0);
-    // Updated math for 3.3V reference
+   
     vOut = (rawValue * 3.3) / 1024.0;
-    // find the lowest vout value
+    
     if (VoutMin > vOut) {
       VoutMin = vOut; 
     }
@@ -30,7 +29,7 @@ void loop() {
   }
   VoutAverage = vOutSum/rangeCount;
 
-  // AD8317 Logic (approximate values)
+  
   float intercept = 2.0; 
   float slope = -0.022; 
   float dBm = (vOut - intercept) / slope;
